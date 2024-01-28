bc534f03ab6a994abe9b86fc1a40a341523870c1
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2024-01-28 16:36:45 -0500

    feat(carousal):added carousal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cloud from "./Pages/services/Cloud.tsx";
 import Analytics from "./Pages/services/Analytics.tsx";
 import Consulting from "./Pages/services/Consulting.tsx";
 import Home from "./Pages/Home.tsx";
 import "./App.css";
-
+import Carousel from "./Carousal.tsx";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
@@ -21,26 +16,38 @@ function App() {
           <Route path="/consulting" element={<Consulting />} />
         </Routes>
       </BrowserRouter>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="flex-container">
+        <Carousel />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        Extended teams GBS is committed to building and maintaining exceptional
+        relationships with our clients and candidates. We have successfully
+        helped many companies identify, interview and on-board talented
+        professionals at all levels. We have strategy-insight driven methods to
+        source professionals for the entire gamut of IT including excellently
+        fitting Cloud Engineers, Project Managers, Business Analysts,
+        Infrastructure Engineers and brilliant Techies for all verticals. We
+        have Passionate and genuinely experienced software professionals with
+        very high skill sets and competencies to take up any given tasks. GBS
+        prioritizes on genuineness of our resources and provide them with equal
+        opportunity to learn, grow and succeed For Employers We provide the
+        right skills at the right price, right when you need it. Finding the
+        right staff for your company is an exhaustive process. Once we
+        understand your needs, we will provide you with carefully screened job
+        candidates. Our supreme knowledge of the technology marketplace allows
+        us to access the very best and brightest in the industry. We will
+        provide you with top quality aspirants, tailored for your company needs
+        and environment, allowing you to interview a select candidate and bring
+        them on-board.We are provide resource to work onsite and remotely We
+        recruit for your needs Our recruiting process is designed to find
+        associates who can add business value in your organization from day one.
+        Through our vast network of contacts, we have unmatched access to a pool
+        of skilled individuals. We use proven and reliable job-related
+        assessments to pinpoint candidates’ abilities and fit with your work
+        environment. And, we work to build an in-depth understanding of your
+        objectives to effectively match our associates to your needs.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/Pages/services/Consulting.tsx
+++ src/Pages/services/Consulting.tsx
@@ -1,5 +1,11 @@
 function Consulting() {
-  return <div>Consulting</div>;
+  return (
+    <div>
+      Technology consulting and Training [Image/description] Our solutions IOT &
+      Digital Engineering [Image/description] Our solutions Product Development
+      and Application Development Services [Image/Descriptions] Our solutions
+    </div>
+  );
 }
 
 export default Consulting;

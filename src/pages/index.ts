f6b3c2d316419ac19acbb81533560659664ec8fe
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2026-02-01 20:19:53 -0600

    changes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,4 @@ export * from './careers/careers';
 export * from './contact/ContactUs';
 export * from './home/Home';
 export * from './products/Product';
-export * from './clients/clients';
 export * from './services';
--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -2,22 +2,14 @@ import './Product.css';
 export function Product() {
   return (
     <div>
-      <div className="productcard">
-        <div className="productImage">Image class</div>
-        <div className="product">
-          <h5>PRODUCTS</h5>
-          <div className="productheader">
-            {' '}
-            This needs to be replaced with icrops.
-          </div>
-          <div className="h3text">
-            Replace with icrops. Designed for companies serious about taking
-            advantage of the benefits of automation, BotzAutomation is a
-            scalable, cost-effective, and enterprise-wide SaaS test automation
-            tool that supports both Agile development and business process
-            teams.
-          </div>
-        </div>
+      <div className="MainHeroCarrers">
+        <div className="heroTextCarrers">Blogs</div>
+      </div>
+      <div className="CareersHero">
+        
+      </div>
+      <div className="ServicePage">
+        Blogs from GBS
       </div>
     </div>
   );
